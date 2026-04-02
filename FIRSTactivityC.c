64541cb6a5e1a054cc43be4e824c0a0f0532f299
Giovanna <giovannasantos.acad@gmail.com>
2026-04-02 14:31:44 -0300

    Incluindo primeiro arquivo C

---
+++ FIRSTactivityC.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+ int macas = 5
+ float preco = 2,50
+ char letra = 'x'
+
+ printf ("Eu tenho %d macas.\n", macas);
+ printf("Cada uma custa R$ %.2F.\N");
+ printf('A letra escolhida foi %c.\\n", letra);
+    
+return 0;
+}
